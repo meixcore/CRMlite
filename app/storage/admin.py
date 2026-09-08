6b2e27fa93a42d627efb8461780c4ff25e68b7b5
meix <meixz@mail.ru>
2026-09-08 13:27:43 +0300

    поменяли AbstractBaseUser на AbstractГыUser, добавили storage в admin, обновили .toml

--- app/storage/admin.py
+++ app/storage/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Storage
+
+
+@admin.register(Storage)
+class StorageAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "address",
+        "company",
+    ]
+
+    search_fields = [
+        "address",
+        "company",
+    ]
